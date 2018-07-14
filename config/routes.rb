bcbfaacdee1c6c56daeaa6c8ac46f74fd1405ebc
nohara <nohara.masato.infratop@gmail.com>
2018-07-15 00:01:13 +0900

    devise and productscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :products, only: [:show, :index]
+namespace :admin do
+  resources :products, only: [:create, :destroy, :update, :edit, :new]
+end
+root :to => 'products#index'
 end
