9a3bbb9b91435554e661c073e13e220fb534f06a
nohara <nohara.masato.infratop@gmail.com>
2018-07-19 19:21:18 +0900

    create purchase-action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,9 @@
 class OrdersController < ApplicationController
+	def show
+		if Order.find(params[:id]).user_id == current_user.id
+			@order_products = Order.find(params[:id]).order_products
+		else
+			redirect_to products_path
+		end
+	end
 end
