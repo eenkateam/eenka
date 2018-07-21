56ebf3bcb0b32bb54b4176b938ce1199ec5fd7cd
m-HirokiOhara <ohara.hiroki.infratop@gmail.com>
2018-07-21 14:50:58 +0000

    User registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, length: { in: 2..20}, presence: true, uniqueness: true
+  validates :last_name, length: { in: 2..20}, presence: true, uniqueness: true
+  validates :first_kana, length: { in: 2..20}, presence: true, uniqueness: true
+  validates :last_kana, length: { in: 2..20}, presence: true, uniqueness: true
+  validates :postal_code, length: { in: 7}, presence: true
+  validates :adress, length: { in: 2..50}, presence: true
+  validates :phone_number, length: { in: 10..11}, presence: true, uniqueness: true
+
   has_one :cart
   has_many :orders
   has_many :receivers
