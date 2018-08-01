3e2e8331a88c1515e481761ff8d6643a59b16e80
nohara <nohara.masato.infratop@gmail.com>
2018-08-02 08:29:33 +0900

    商品詳細ページのレイアウトを整えた

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -3,5 +3,5 @@ class Receiver < ApplicationRecord
 	has_many :orders
 	has_one :cart
 	validates :receiver_adress, length: { in: 2..50}, presence: true
-	validates :receiver_name length: { in: 2..50}, presence: true
+	validates :receiver_name, length: { in: 2..50}, presence: true
 end
