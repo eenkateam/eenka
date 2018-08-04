79e3534892f3b4ccd6ed59c2d716171287060370
nohara <nohara.masato.infratop@gmail.com>
2018-08-04 16:48:40 +0900

    レシーバーのエラーを日本語にした

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -2,6 +2,6 @@ class Receiver < ApplicationRecord
 	belongs_to :user
 	has_many :orders
 	has_one :cart
-	validates :receiver_adress, length: { in: 2..50}, presence: true
-	validates :receiver_name, length: { in: 2..50}, presence: true
+	validates :receiver_adress, presence: true
+	validates :receiver_name, presence: true
 end
