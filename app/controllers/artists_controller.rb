97293b7ca6f08feff2e852d4b535643ca94bac89
NishikawaYuzuru <nishikawa.yuzuru.infratop@gmail.com>
2018-08-09 08:53:29 +0000

    artist show modified

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,5 +4,6 @@ class ArtistsController < ApplicationController
 	end
 	def show
 		@artist = Artist.find(params[:id])
+		@artistProducts = @artist.products.without_soft_destroyed.page(params[:page]).order("id DESC")
 	end
 end
