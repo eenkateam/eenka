7a2cd55a427ebd669062a9b383c42ac6f768691a
nohara <nohara.masato.infratop@gmail.com>
2018-08-10 23:12:18 +0900

    footer

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
-  # registrations: 'admins/registrations'
+  registrations: 'admins/registrations'
 }
 devise_for :users, controllers: {
   sessions:      'users/sessions',
